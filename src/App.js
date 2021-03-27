05f69e5c48b035e9f707344c677362e8033685c4
Manuel F. Espuch Sanchez <mfespuch@gmail.com>
2021-03-27 07:43:01 +0100

    mas avances

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { useState } from 'react';
 import './App.css';
+import ListOfGifs from './components/ListOfGifs';
+import { Link, Route } from 'wouter';
 
-const GIFS = [
-  'https://media3.giphy.com/media/DRYU7xgNIJbzQjOOBH/200w.webp?cid=ecf05e47yx7q6e7agxcqcl3l59hw61ght57atrdniyx8bvd7&rid=200w.webp',
-  'https://media4.giphy.com/media/0Vv0Ne2CnOClIExIuL/200w.webp?cid=ecf05e47yx7q6e7agxcqcl3l59hw61ght57atrdniyx8bvd7&rid=200w.webp'
-];
 
 function App() {
-  const [gifs, setGifs] = useState(GIFS);
   return (
     <div className="App">
       <section className="App-content">
-        {
-          gifs.map(singleGif => <img src={singleGif} />)
-        }
+        <h1>App</h1>
+        <Link to="/gif/colombia">Gifs de colombia</Link>
+        <Route
+          component={ListOfGifs}
+          path="/gif/:keyword"
+        />
+        {/* <ListOfGifs keyword='ecuador' /> */}
       </section>
     </div>
   );
